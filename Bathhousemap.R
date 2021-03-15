3c9551187f641f28fe420afcd55784621ecfba62
adivea <adela@fedarch.org>
2021-03-15 13:08:08 +0100

    added grid to leaflet

--- Bathhousemap.R
+++ Bathhousemap.R
@@ -84,3 +84,52 @@ Bathsmap %>% addPolygons(data=suburbs)
 library(htmlwidgets)
 saveWidget(Bathsmap, "Bathhousemap.html", selfcontained = TRUE)
 
+#############################################################
+#  Part II - Compare OSM data with present dataset
+#############################################################
+# Convert data points into a grid using the function defined below
+
+pt_in_grid <- function(feat, adm, cellsize = 1000){
+  grid <- st_make_grid(x = adm, cellsize = cellsize, what = "polygons")
+  . <- st_intersects(grid, adm)
+  grid <- grid[sapply(X = ., FUN = length)>0]
+  . <- st_intersects(grid, feat)
+  grid <- st_sf(n = sapply(X = ., FUN = length), grid)
+  return(grid)
+}
+
+
+library(leaflet)
+gr_osm <- pt_in_grid(baths_osm,st_transform(suburbs,32632),500)
+gr_his <- pt_in_grid(hist_baths,st_transform(suburbs,32632),500)
+plot(gr_osm)
+plot(gr_his)
+
+grid <- gr_osm %>% rename(osm=n) %>% 
+  mutate(history = gr_his$n) %>% 
+  filter(history==0,osm==0) %>%
+  mutate(missing =  pmax(history - osm,0))
+
+plot(grid)
+which(grid$missing==0)
+which(grid$missing>0)
+which(is.na(grid$missing))
+
+
+bks = c(0,1,2,3,4,5,6,7)
+pal <- colorNumeric(
+  palette = "Greens",
+  domain = gr_osm$n)
+color.scale <- colorBin(pal, domain = range(bks), bins=bks)
+
+leaflet(gr_osm%>% 
+          st_transform(4326) %>% 
+          filter(!is.na(n))) %>% 
+  addProviderTiles(providers$Stamen.TonerLite) %>% 
+  addPolygons(fillColor = ~pal(gr_osm$n), 
+              stroke = FALSE, 
+              fillOpacity = 0.7) %>% 
+  # addLegend(colors = ~pal(bks), 
+  #           #labels = round(bks[1:(length(bks)-1)]*10)/10,
+  #           title = "# net difference between</br> bathrooms 
+  #           in </br>historical records and OSM")
